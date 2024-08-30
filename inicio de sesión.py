2814c76916dfefe9df9c86a56227a38815572e41
neclive <matejajav@gmail.com>
2024-08-30 13:06:35 -0500

    Inicio de sesión a la agencia.
    Utilicé diccionarios, funciones y condicionales.

---
+++ inicio de sesión.py
@@ -0,0 +1,38 @@
+## Inicia la sesión personalizada. Se le solicita al usuario su nombre. 
+## Diccionario vacío, para que guarde temporalmente usuarios.
+
+usuarios_registrados = {}
+
+def iniciar_sesion():
+    usuario = input("Ingrese su nombre de usuario: ")
+    if usuario in usuarios_registrados:
+        contraseña = input("Ingrese su contraseña: ")
+        if usuarios_registrados[usuario] == contraseña:
+            print("¡Inicio de sesión exitoso!")
+        else:
+            print("Contraseña incorrecta.")
+    else:
+        print("Usuario no registrado.")
+        registrar_usuario()
+
+def registrar_usuario():
+    usuario = input("Ingrese un nombre de usuario nuevo: ")
+    if usuario in usuarios_registrados:
+        print("Este nombre de usuario ya está registrado.")
+    else:
+        contraseña = input("Ingrese una contraseña: ")
+        usuarios_registrados[usuario] = contraseña
+        print("¡Registro exitoso!")
+
+# Si el usuario responde como no debe
+print("Bienvenido a la agencia NNNNN la mejor opción para ti y tu familia.")
+while True:
+    opcion = input("¿Desea iniciar sesión (I) o registrarse (R)? ").upper()
+    if opcion == "I":
+        iniciar_sesion()
+        break
+    elif opcion == "R":
+        registrar_usuario()
+        break
+    else:
+        print("Opción inválida. Por favor, seleccione 'I' para iniciar sesión o 'R' para registrarse.")
